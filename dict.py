87442d2384d4e4b93b85f9a671398b9f7e1fcbff
M1k3 Yang <yang.ye.cn@gmail.com>
2013-04-20 23:25:09 +0800

    fix dict.py per PEP8

--- dict.py
+++ dict.py
@@ -5,24 +5,24 @@ from sys import exit
 
 #handle argumments
 if len(argv) == 1:
-	exit("usage: dict the word query against internet.")
+    exit("usage: dict the word query against internet.")
 script, word = argv[0], argv[1:]
-cword = reduce(lambda x,y: str(x) + " " + str(y), word)
+cword = reduce(lambda x, y: str(x) + " " + str(y), word)
 
 #BeautifulSoup4 preparation
-html = urllib2.urlopen('http://dict.cn/'+cword).read() 
-soup = bs(html,"lxml")
+html = urllib2.urlopen('http://dict.cn/'+cword).read()
+soup = bs(html, "lxml")
 
 #searching for the word
-answer = soup.find('div',"layout basic")
+answer = soup.find('div', "layout basic")
 # if not search for english word, it should be chinese word which have a different layout
-if answer == None:
-	answer = soup.find('div',"layout cn")
-# 
-if answer == None:
-	exit("cannot found definition.")	
+if answer is None:
+    answer = soup.find('div', "layout cn")
+# situation of cannot find any definitation both way.
+if answer is None:
+    exit("cannot found definition.")
 for x in answer.find_all('li'):
-	try:
-		print(x.text)
-	except Exception as inst:
-		continue
+    try:
+        print(x.text)
+    except Exception as inst:
+        continue
