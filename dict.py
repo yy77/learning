de0c353ef612feaf36fd4c585c81045ab3caabce
M1k3 Yang <yang.ye.cn@gmail.com>
2013-07-01 11:04:19 +0800

    fix css selector for dict.cn

--- dict.py
+++ dict.py
@@ -3,22 +3,15 @@ import urllib2
 from sys import argv
 from sys import exit
 
-#handle argumments
 if len(argv) == 1:
 	exit("usage: dict the word query against internet.")
 script, word = argv[0], argv[1:]
 cword = reduce(lambda x,y: str(x) + " " + str(y), word)
-
-#BeautifulSoup4 preparation
 html = urllib2.urlopen('http://dict.cn/'+cword).read() 
 soup = bs(html,"lxml")
-
-#searching for the word
-answer = soup.find('div',"layout basic")
-# if not search for english word, it should be chinese word which have a different layout
+answer = soup.find('div',"layout basic clearfix")
 if answer == None:
 	answer = soup.find('div',"layout cn")
-# 
 if answer == None:
 	exit("cannot found definition.")	
 for x in answer.find_all('li'):
